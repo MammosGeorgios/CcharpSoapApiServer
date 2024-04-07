e5c9214cf6d5c48e0521a4dfa612729c099c773b
GMAMMOS001 <giorgos.mammos@pwc.com>
2024-04-07 14:57:00 +0300

    SoapTestApplication - Initial commit

---
+++ SoapTestApplication/Program.cs
@@ -0,0 +1,19 @@
+using SoapCore;
+using SoapTestApplication.BusinessLogic;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSoapCore();
+builder.Services.AddScoped<ISoapService, SoapService>();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.UseSoapEndpoint<ISoapService>("/Service.asmx", new SoapEncoderOptions(),
+        SoapSerializer.XmlSerializer);
+});
+
+app.Run();
